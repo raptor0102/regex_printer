1fdc3c85dadea3071e9dd444386dae3d83b24fb7
raptor0102 <yahavv@gmail.com>
2020-11-15 05:15:07 +0200

    Final RegexPrinter project code to github
    Signed-off-by: raptor0102 <yahavv@gmail.com>

--- src/tt.py
+++
@@ -1,5 +0,0 @@
-import glob
-yy=glob.glob('C:\\Users\\Nadav\\connection\\connection_projects\\project\Docker\\tests\\*.txt')
-for fi in yy:
-    print(fi.split('\\')[-1])
-# print(glob.glob('C:\\Users\\Nadav\\connection\\connection_projects\\project\Docker\\src\\*.py'))
